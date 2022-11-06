dcb21eaa8d50394ce8a8423751411f7579c8545c
memesnopower <115490701+memesnopower@users.noreply.github.com>
2022-11-06 15:54:15 +0300

    Update homework.cpp

--- homework.cpp
+++ homework.cpp
@@ -1,61 +1,114 @@
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <random>
+#include <vector>
 #include <cstring>
+#include <cmath>
+#include <random>
 
 using namespace std;
 
-// есть маска которая генерируется с помощью программироваемого рандома где нужен ключ
+int left_shift(int x, int bytes = 4) {
+    return (x << 3) | (x >> (bytes*8-3));
+}
 
-void encryption(int key, char *words) {
-    for (int i = 0; i < strlen(words); i++) {
-        words[i] ^= key;
+int right_shift(int x, int bytes = 4) {
+    return (x >> 3) | (x << (bytes*8-3));
+}
+int gamma, gamma2;
+void init(){
+
+    srand(12331);
+    gamma = rand();
+
+
+}
+
+
+void encryption(string &str, string &decrypt) {
+    int result = 0;
+    int size = 4;
+    int incomplete_blocks = str.size() % 4;
+    int blocks = str.size() / 4;
+    int maska;
+    char sentences[str.size() + 1];
+
+    for (int i = 0; i < str.size(); i++) {
+        sentences[i] = str[i];
     }
-    cout << "Encryption message: " << words;
-}
 
-void decryption(int key, char *words) {
-    for (int i = 0; i < strlen(words); i++) {
-        words[i] ^= key;
+    for (int i = 0; i <= str.size(); i++) {
+        if (i < blocks) {
+            memcpy(&result, sentences + i * 4, size);
+
+            result = left_shift(result ^ gamma);
+
+
+            memcpy(sentences + i * 4, &result, size);
+        } else {
+            memcpy(&result, sentences + i * 4, incomplete_blocks);
+
+            result = left_shift(result, incomplete_blocks);
+            result ^= (gamma >> (8 * (4 - incomplete_blocks)));
+            memcpy(sentences + i * 4, &result, incomplete_blocks);
+        }
     }
-    cout << "Decryption message: " << words;
-
+    decrypt = sentences;
+    cout << decrypt;
 }
-/*
-int shift_left(int left, int bytes = 4) {
 
+void decryption(string &decrypt, string &str) {
+    int result = 0;
+    int size = 4;
+    int incomplete_blocks = decrypt.size() % 4;
+    int blocks = decrypt.size() / 4;
+    int maska;
 
+    char sentences[decrypt.size() + 1];
+
+    for (int i = 0; i < decrypt.size(); i++) {
+        sentences[i] = decrypt[i];
+    }
+
+    for (int i = 0; i <= decrypt.size(); i++) {
+        if (i < blocks) {
+            memcpy(&result, sentences + i * 4, size);
+
+            result = right_shift(result);
+            result ^= gamma;
+
+            memcpy(sentences + i * 4, &result, size);
+
+        } else {
+            memcpy(&result, sentences + i * 4, incomplete_blocks);
+
+            result = right_shift(result, incomplete_blocks);
+            result ^= (gamma >> (8 * (4 - incomplete_blocks)));
+            memcpy(sentences + i * 4, &result, incomplete_blocks);
+
+        }
+    }
+    str = sentences;
+    cout << str;
 }
-*/
 
-int main(int argc, char *argv[]) {
-/*
-    if (argc != 3) {
+int main(int argv, char *argc[]) {
+    init();
+    /*
+    if (argv != 3) {
         cerr << "Use three parameters!";
 
-        return 5;
+        return 7;
     }
-*/
-    const string MODE = argv[1];
-    const string FILE_NAME = argv[2];
-
-
-/*
-    if (FILE_NAME.is_open() == false) {
-        cerr << "Program can not open file";
-        return -1;
-    }
-*/
-
-    if (MODE == "encryption") {
-        encryption(50, "dsnnddn");
-    } else if (MODE == "decryption") {
-            //decryption();
-    } else {
-        cerr << "Use encryption or decryption only!";
-        return 5;
-    }
+    */
+    string str = "hey, how are you", decrypt = "╤└╦cУк{╗УтУ+У\"{л";
+    /*
+    const string MODE = argc[1];
+    const string FILE_NAME = argc[2];
+    ifstream file;
+    file.open(FILE_NAME);
+    */
+    encryption(str, decrypt); cout << endl;
+    decryption(decrypt, str);
 
     return 0;
 }
